6b2daaf3e76b8648f69ac7e4090e953cf3236029
Yerquat <yernur75@gmail.com>
2022-11-29 00:33:29 +0600

    Example005 Condition if-else/

---
+++ Example005_Condition_if-else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "еркуат")
+{
+    Console.WriteLine("Ура, это Еркуат");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
